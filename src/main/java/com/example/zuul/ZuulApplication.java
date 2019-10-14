164f43da0bd9c6bef0736511292412182a490728
JimDa <1085529137@qq.com>
2019-10-14 23:37:36 +0800

    简单路由

--- src/main/java/com/example/zuul/ZuulApplication.java
+++ src/main/java/com/example/zuul/ZuulApplication.java
@@ -2,16 +2,14 @@ package com.example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableConfigurationProperties
+//@EnableConfigurationProperties(IgnoredUrlsConfig.class)
 public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
-
 }
--- src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
+++ src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
@@ -1,34 +1,25 @@
-package com.example.zuul.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
-@ConfigurationProperties(prefix = "allowed.routes")
-public class IgnoredUrlsConfig {
-    private List<String> path = new ArrayList<>();
-    private String haha;
-
-    public String[] getUrls() {
-        String[] urlArr = new String[this.path.size()];
-        for (int i = 0; i < urlArr.length; i++) {
-            urlArr[i] = this.path.get(i);
-        }
-        return urlArr;
-    }
-
-    public List<String> getPath() {
-        return path;
-    }
-
-    public String getHaha() {
-        return haha;
-    }
-
-    public void setHaha(String haha) {
-        this.haha = haha;
-    }
-}
+//package com.example.zuul.config;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+////@Component
+//@ConfigurationProperties(prefix = "allowed.routes")
+//@Setter
+//@Getter
+//public class IgnoredUrlsConfig {
+//    private List<String> path = new ArrayList<>();
+//
+//    public String[] getUrls() {
+//        String[] urlArr = new String[this.path.size()];
+//        for (int i = 0; i < urlArr.length; i++) {
+//            urlArr[i] = this.path.get(i);
+//        }
+//        return urlArr;
+//    }
+//}
