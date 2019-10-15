038b279e6cee29284721667cdadb2ff9a6ab4952
JimDa <1085529137@qq.com>
2019-10-15 23:10:50 +0800

    修改

--- src/main/java/com/example/zuul/ZuulApplication.java
+++ src/main/java/com/example/zuul/ZuulApplication.java
@@ -1,15 +1,34 @@
 package com.example.zuul;
 
+import com.example.zuul.config.IgnoredUrlsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
-//@EnableConfigurationProperties(IgnoredUrlsConfig.class)
+@EnableConfigurationProperties
+@RestController
 public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean(name = "ignoredUrls")
+    @ConfigurationProperties(prefix = "allowed.routes")
+    public IgnoredUrlsConfig getIgnoredUrls() {
+        return new IgnoredUrlsConfig();
+    }
+
+    @GetMapping(value = "/urls")
+    public ResponseEntity<IgnoredUrlsConfig> getUrls() {
+        return ResponseEntity.ok(getIgnoredUrls());
     }
 }
--- src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
+++ src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
@@ -1,25 +1,45 @@
-//package com.example.zuul.config;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
+package com.example.zuul.config;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 ////@Component
 //@ConfigurationProperties(prefix = "allowed.routes")
 //@Setter
 //@Getter
-//public class IgnoredUrlsConfig {
-//    private List<String> path = new ArrayList<>();
-//
-//    public String[] getUrls() {
-//        String[] urlArr = new String[this.path.size()];
-//        for (int i = 0; i < urlArr.length; i++) {
-//            urlArr[i] = this.path.get(i);
-//        }
-//        return urlArr;
-//    }
-//}
+
+//@Data
+public class IgnoredUrlsConfig {
+    private String all;
+    private List<String> path = new ArrayList<>();
+
+    public String[] getUrls() {
+        String[] urlArr = new String[this.path.size()];
+        for (int i = 0; i < urlArr.length; i++) {
+            urlArr[i] = this.path.get(i);
+        }
+        return urlArr;
+    }
+
+    public List<String> getPath() {
+        return path;
+    }
+
+    public void setPath(List<String> path) {
+        this.path = path;
+    }
+
+    public String getAll() {
+        return all;
+    }
+
+    public void setAll(String all) {
+        this.all = all;
+    }
+}
