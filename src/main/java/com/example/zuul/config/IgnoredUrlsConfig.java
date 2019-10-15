9b30a185a60fc8a375381eacf3f710c0d864183a
dpc <dapengcheng@indexvc.com>
2019-10-15 16:50:18 +0800

    cors config

--- src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
+++ src/main/java/com/example/zuul/config/IgnoredUrlsConfig.java
@@ -1,25 +1,29 @@
-//package com.example.zuul.config;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-////@Component
-//@ConfigurationProperties(prefix = "allowed.routes")
-//@Setter
-//@Getter
-//public class IgnoredUrlsConfig {
-//    private List<String> path = new ArrayList<>();
-//
-//    public String[] getUrls() {
-//        String[] urlArr = new String[this.path.size()];
-//        for (int i = 0; i < urlArr.length; i++) {
-//            urlArr[i] = this.path.get(i);
-//        }
-//        return urlArr;
-//    }
-//}
+package com.example.zuul.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "allowed.routes")
+@Setter
+@Getter
+@EnableConfigurationProperties
+@Configuration
+public class IgnoredUrlsConfig {
+    private List<String> path = new ArrayList<>();
+
+    public String[] getUrls() {
+        String[] urlArr = new String[this.path.size()];
+        for (int i = 0; i < urlArr.length; i++) {
+            urlArr[i] = this.path.get(i);
+        }
+        return urlArr;
+    }
+}
